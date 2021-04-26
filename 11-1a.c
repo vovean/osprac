5659fcbf928c9df222794e06281bcb0ee0420270
vovean <vislovichv@mail.ru>
2021-04-26 14:08:40 +0300

    task 1 done

--- 11-1a.c
+++ 11-1a.c
@@ -1,5 +1,6 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
+#include <sys/types.h>
 #include <sys/msg.h>
 #include <string.h>
 #include <stdio.h>
@@ -7,64 +8,67 @@
 
 #define LAST_MESSAGE 255 // Message type for termination of program 11-1b.c
 
-int main(void)
-{
-  int msqid;      // IPC descriptor for the message queue
-  char pathname[]="11-1a.c"; // The file name used to generate the key.
-                             // A file with this name must exist in the current directory.
-  key_t  key;     // IPC key
-  int i,len;      // Cycle counter and the length of the informative part of the message
+int main(void) {
+    int msqid;      // IPC descriptor for the message queue
+    char pathname[] = "11_1a.c"; // The file name used to generate the key.
+    // A file with this name must exist in the current directory.
+    key_t key;     // IPC key
+    int len;      // Cycle counter and the length of the informative part of the message
 
-  struct mymsgbuf // Custom structure for the message
-  {
-    long mtype;
-    char mtext[81];
-  } mybuf;
+    struct mymsgbuf // Custom structure for the message
+    {
+        long mtype;
+        struct {
+            short sinfo;
+            float finfo;
+        } info;
+    } mybuf;
 
-  if ((key = ftok(pathname,0)) < 0) {
-    printf("Can\'t generate key\n");
-    exit(-1);
-  }
-  //
-  // Trying to get access by key to the message queue, if it exists,
-  // or create it, with read & write access for all users.
-  //
-  if ((msqid = msgget(key, 0666 | IPC_CREAT)) < 0) {
-    printf("Can\'t get msqid\n");
-    exit(-1);
-  }
-
-  /* Send information */
-
-  for (i = 1; i <= 5; i++) {
-    //
-    // Fill in the structure for the message and
-    // determine the length of the informative part.
-    //
-    mybuf.mtype = 1;
-    strcpy(mybuf.mtext, "This is text message");
-    len = strlen(mybuf.mtext)+1;
-    //
-    // Send the message. If there is an error,
-    // report it and delete the message queue from the system.
-    //
-    if (msgsnd(msqid, (struct msgbuf *) &mybuf, len, 0) < 0) {
-      printf("Can\'t send message to queue\n");
-      msgctl(msqid, IPC_RMID, (struct msqid_ds *) NULL);
-      exit(-1);
+    if ((key = ftok(pathname, 0)) < 0) {
+        printf("Can\'t generate key\n");
+        exit(-1);
     }
-  }
+    //
+    // Trying to get access by key to the message queue, if it exists,
+    // or create it, with read & write access for all users.
+    //
+    if ((msqid = msgget(key, 0666 | IPC_CREAT)) < 0) {
+        printf("Can\'t get msqid\n");
+        exit(-1);
+    }
 
-  /* Send the last message */
+    /* Send information */
 
-  mybuf.mtype = LAST_MESSAGE;
-  len         = 0;
+    for (short i = 1; i <= 5; i++) {
+        //
+        // Fill in the structure for the message and
+        // determine the length of the informative part.
+        //
+        mybuf.mtype = i;
+        mybuf.info.sinfo = i;
+        mybuf.info.finfo = (float) i;
+        len = sizeof(mybuf.info);
+        //
+        // Send the message. If there is an error,
+        // report it and delete the message queue from the system.
+        //
+        if (msgsnd(msqid, &mybuf, len, 0) < 0) {
+            printf("Can\'t send message to queue\n");
+            msgctl(msqid, IPC_RMID, (struct msqid_ds *) NULL);
+            exit(-1);
+        }
+    }
 
-  if (msgsnd(msqid, (struct msgbuf *) &mybuf, len, 0) < 0) {
-    printf("Can\'t send message to queue\n");
-    msgctl(msqid, IPC_RMID, (struct msqid_ds *) NULL);
-    exit(-1);
-  }
+    /* Send the last message */
 
-  return 0;
+    mybuf.mtype = LAST_MESSAGE;
+    len = 0;
+
+    if (msgsnd(msqid, &mybuf, len, 0) < 0) {
+        printf("Can\'t send message to queue\n");
+        msgctl(msqid, IPC_RMID, (struct msqid_ds *) NULL);
+        exit(-1);
+    }
+
+    return 0;
 }
--- 11-1b.c
+++ 11-1b.c
@@ -1,5 +1,6 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
+#include <sys/types.h>
 #include <sys/msg.h>
 #include <string.h>
 #include <stdio.h>
@@ -7,53 +8,54 @@
 
 #define LAST_MESSAGE 255
 
-int main(void)
-{
-  int msqid;
-  char pathname[]="11-1a.c";
-  key_t  key;
-  int len, maxlen;
+int main(void) {
+    int msqid;
+    char pathname[] = "11_1a.c";
+    key_t key;
+    int maxlen;
 
-  struct mymsgbuf
-  {
-    long mtype;
-    char mtext[81];
-  } mybuf;
+    struct mymsgbuf {
+        long mtype;
+        struct {
+            short sinfo;
+            float finfo;
+        } info;
+    } mybuf;
 
-  if ((key = ftok(pathname,0)) < 0) {
-    printf("Can\'t generate key\n");
-    exit(-1);
-  }
-
-  if ((msqid = msgget(key, 0666 | IPC_CREAT)) < 0) {
-    printf("Can\'t get msqid\n");
-    exit(-1);
-  }
-
-  while (1) {
-    //
-    // In an infinite loop, accept messages of any type in FIFO order
-    // with a maximum length of 81 characters
-    // until a message of type LAST_MESSAGE is received.
-    //
-    maxlen = 81;
-
-    if (( len = msgrcv(msqid, (struct msgbuf *) &mybuf, maxlen, 0, 0)) < 0) {
-      printf("Can\'t receive message from queue\n");
-      exit(-1);
-    }
-    //
-    // If the received message is of type LAST_MESSAGE,
-    // then terminate and remove the message queue from the system.
-    // Otherwise, print the text of the received message.
-    //
-    if (mybuf.mtype == LAST_MESSAGE) {
-      msgctl(msqid, IPC_RMID, (struct msqid_ds *)NULL);
-      exit(0);
+    if ((key = ftok(pathname, 0)) < 0) {
+        printf("Can\'t generate key\n");
+        exit(-1);
     }
 
-    printf("message type = %ld, info = %s\n", mybuf.mtype, mybuf.mtext);
-  }
+    if ((msqid = msgget(key, 0666 | IPC_CREAT)) < 0) {
+        printf("Can\'t get msqid\n");
+        exit(-1);
+    }
 
-  return 0;
+    while (1) {
+        //
+        // In an infinite loop, accept messages of any type in FIFO order
+        // with a maximum length of 81 characters
+        // until a message of type LAST_MESSAGE is received.
+        //
+        maxlen = sizeof(mybuf.info);
+
+        if (msgrcv(msqid, &mybuf, maxlen, 0, 0) < 0) {
+            printf("Can\'t receive message from queue\n");
+            exit(-1);
+        }
+        //
+        // If the received message is of type LAST_MESSAGE,
+        // then terminate and remove the message queue from the system.
+        // Otherwise, print the text of the received message.
+        //
+        if (mybuf.mtype == LAST_MESSAGE) {
+            msgctl(msqid, IPC_RMID, (struct msqid_ds *) NULL);
+            exit(0);
+        }
+
+        printf("message type = %ld, sinfo = %hd, finfo = %f\n", mybuf.mtype, mybuf.info.sinfo, mybuf.info.finfo);
+    }
+
+    return 0;
 }
